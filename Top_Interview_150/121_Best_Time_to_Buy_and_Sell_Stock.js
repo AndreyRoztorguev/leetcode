2d3eade436995e012b5bd847962435496a6e17be
Andrey <andrew.roztor99@gmail.com>
2024-11-21 01:11:53 +0200

    121_Best_Time_to_Buy_and_Sell_Stock.js

---
+++ Top_Interview_150/121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = Infinity; // To store the minimum price encountered
+  let maxProfit = 0; // To store the maximum profit
+
+  for (let i = 0; i < prices.length; i++) {
+    // If we find a new minimum price, update it
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    }
+    // Calculate profit if sold at the current price
+    const profit = prices[i] - minPrice;
+    // Update the max profit if we find a higher profit
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+  }
+
+  return maxProfit;
+};
