1fff59fbf7440232e23d9ec6c75d5b051160fe7b
Andrey <andrew.roztor99@gmail.com>
2024-11-24 23:34:22 +0200

    238_Product_of_Array_Except_Self.js

---
+++ Top_Interview_150/238_Product_of_Array_Except_Self.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  const output = new Array(nums.length).fill(1);
+  let leftProduct = 1;
+  for (let i = 0; i < nums.length; i++) {
+    output[i] = leftProduct;
+    leftProduct *= nums[i];
+  }
+  let rightProduct = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    output[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+  return output;
+};
